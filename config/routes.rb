14b70b035ed43c2780fac792d16e96b52cb232d4
Yair <ycosio@ucol.mx>
2017-05-01 18:20:55 -0500

    Controller and view implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'band/index'
+
   root 'home#index'
 
   resources :home
   resources :about_me
+  resources :band
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
